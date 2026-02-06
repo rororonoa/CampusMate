93f25f7bcf26bcc70ed0510de43f2acc10d48154
Sumeet Shetty <shettysumeet33@gmail.com>
2026-02-07 00:48:24 +0530

    PORT message changed

--- backend/server.js
+++ backend/server.js
@@ -56,5 +56,5 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`CampusMate backend running on port ${PORT}`);
 });
